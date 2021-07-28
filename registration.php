855ba5d2706fb75efcf23f6e9b67556a55109710
tabibaladib <78272853+tabibaladib@users.noreply.github.com>
2021-07-28 23:04:05 +0600

    Updated Registration Form

--- registration.php
+++ registration.php
@@ -22,61 +22,62 @@
 	$fnameErr = $lnameErr = $genderErr = $dobErr = $religionErr = $emailErr = $usernameErr = $passwordErr = "";
 
 	$successMessage = $errMessage ="";
-	$flag = false;
-	
+	$isValid = true;	
+
 	   if($_SERVER['REQUEST_METHOD'] === "POST") 
 	   	{ 
+	      	
 	   	  if(empty($_POST['fname']))
 	       {
             $fnameErr = "First Name can not be empty!";
-            $flag = true;
+            $isValid = false;
 	       }
 
 	       if(empty($_POST['lname']))
 	       {
             $lnameErr = "Last Name can not be empty!";
-            $flag = true;
+            $isValid = false;
 	       }
 
 	       if(empty($_POST['gender']))
 	       {
             $genderErr = "Gender can not be empty!";
-            $flag = true;
+            $isValid = false;
 	       }
 
           if(empty($_POST['dob']))
 	       {
             $dobErr = "Date of Birth can not be empty!";
-            $flag = true;
+            $isValid = false;
 	       }
 
 	       if(empty($_POST['religion']))
 	       {
             $religionErr = "Religion can not be empty!";
-            $flag = true;
+            $isValid = false;
 	       }
 
 	       if(empty($_POST['email']))
 	       {
             $emailErr = "Email can not be empty!";
-            $flag = true;
+            $isValid = false;
 	       }
 
 	       if(empty($_POST['username']))
 	       {
             $usernameErr = "Username can not be empty!";
-            $flag = true;
+            $isValid = false;
 	       }
 
 	       if(empty($_POST['password']))
 	       {
             $passwordErr = "Password can not be empty!";
-            $flag = true;
+            $isValid = false;
 	       }
 
-	       if(!$flag)
+	       if($isValid)
 	       {
-	      	$fname = $_POST['fname'];
+	       	$fname = $_POST['fname'];
             $lname = $_POST['lname'];
           	$gender = $_POST['gender'];
             $dob = $_POST['dob'];
@@ -88,7 +89,7 @@
             $pw = $_POST['pw'];
             $username = $_POST['username'];
             $password = $_POST['password'];
-	      	
+
 	      	$fname = test_input($fname);
 	      	$lname = test_input($lname);
 	      	$gender = test_input($gender);
@@ -104,19 +105,21 @@
 
 
             $response = register($fname, $lname, $gender, $dob, $religion, $pra, $pa, $phone, $email, $pw, $username, $password);
+
             if($response)
             {
                $successMessage = "User Registered Successfully!";
             }
 
-
             
 	       }
 
-	      else
-	      {
-	      	$errMessage = "Error while saving!";
-	      }
+	       else
+	         {
+	      	   $errMessage = "Error while saving!";
+	         }
+
+	      
 
 	    }
  
@@ -136,12 +139,12 @@
 	   <legend>Basic Information:</legend>
 	   <label for="fname">First name: </label> 
 	   <input type="text" id="fname" name="fname" value="<?php //echo $fname;?>">
-	   <span style="color: red;"><?php echo $fnameErr; ?></span>
+	   <span style="color: red;" id = "fnameErr"><?php echo $fnameErr; ?></span>
 	   <br><br>
 
 	   <label for="lname">Last name: </label> 
 	   <input type="text" id="lname" name="lname" value="<?php //echo $lname;?>">
-	   <span style="color: red;"><?php echo $lnameErr; ?></span>
+	   <span style="color: red;" id = "lnameErr"><?php echo $lnameErr; ?></span>
 	   <br> <br>
 
 	   <label for="gender">Gender: </label>
@@ -149,17 +152,18 @@
 	   <label for="male">Male</label>
 	   <input type="radio" id="female" name="gender" value="female">
 	   <label for="female">Female</label>
-	   <span style="color: red;"><?php echo $genderErr; ?></span>
+	   <span style="color: red;" id = "genderErr"><?php echo $genderErr; ?></span>
 	   <br><br>
 
 	   <label for="dob">DOB:</label>
        <input type="date" id="dob" name="dob">
-       <span style="color: red;"><?php echo $dobErr; ?></span>
+       <span style="color: red;" id = "dobErr"><?php echo $dobErr; ?></span>
 	   <br><br>
 
 	   <label for="religion">Religion: </label>
 	   <select name = "religion" id="religion">
-	   	<option value="none" selected disabled hidden>Select an option
+	   	<option value="none" selected disabled hidden>Select 
+
       </option>
 	  	<option value = "islam">Islam</option>
 	  	<option value = "hindu">Hindu</option>
@@ -167,7 +171,7 @@
 	  	<option value = "christianity">Christianity</option>
 	  	<option value = "other">Other</option>
 	   </select>
-	   <span style="color: red;"><?php echo $religionErr; ?></span>
+	   <span style="color: red;" id = "religionErr"><?php echo $religionErr; ?></span>
 
 	   </fieldset>
 
@@ -188,7 +192,7 @@
 
 	   <label for="email">Email: </label>
 	   <input type="email" id="email" name="email">
-	   <span style="color: red;"><?php echo $emailErr; ?></span>
+	   <span style="color: red;" id = "emailErr"><?php echo $emailErr; ?></span>
 	   <br> <br>
 
 	   <label for="pw">Personal Website: </label> 
@@ -201,12 +205,12 @@
 	   <legend> Account Information:</legend>
 	   <label for="username">Username: </label> 
 	   <input type="text" id="username" name="username">
-	   <span style="color: red;"><?php echo $usernameErr; ?></span>
+	   <span style="color: red;" id = "usernameErr"><?php echo $usernameErr; ?></span>
 	   <br> <br>
 
 	   <label for="password">Password: </label> 
 	   <input type="password" id="password" name="password">
-	   <span style="color: red;"><?php echo $passwordErr; ?></span>
+	   <span style="color: red;" id = "passwordErr"><?php echo $passwordErr; ?></span>
 	   <br>
 
 	   </fieldset>
@@ -218,8 +222,20 @@
 </form>
 
 <script>
-	function isvalid() 
+	function isValid() 
 	{
+		var flag = true;
+
+		var fnameErr = document.getElementById("fnameErr");
+		var lnameErr = document.getElementById("lnameErr");
+		var genderErr = document.getElementById("genderErr");
+		var dobErr = document.getElementById("dobErr");
+		var religionErr = document.getElementById("religionErr");
+		var emailErr = document.getElementById("emailErr");
+		var usernameErr = document.getElementById("usernameErr");
+		var passwordErr = document.getElementById("passwordErr");
+
+
 		var fname = document.forms["regform"]["fname"].value;
 		var lname = document.forms["regform"]["lname"].value;
 		var gender = document.forms["regform"]["gender"].value;
@@ -228,7 +244,67 @@
 		var email = document.forms["regform"]["email"].value;
 		var username = document.forms["regform"]["username"].value;
 		var password = document.forms["regform"]["password"].value;
-		return false;
+
+		fnameErr.innerHTML = "";
+		lnameErr.innerHTML = "";
+		genderErr.innerHTML = "";
+		dobErr.innerHTML = "";
+		religionErr.innerHTML = "";
+		emailErr.innerHTML = "";
+		usernameErr.innerHTML = "";
+		passwordErr.innerHTML = "";
+
+		if(fname === "")
+		{
+			flag = false;
+			document.getElementById("fnameErr").innerHTML = "First Name can't be empty!";
+		}
+
+		if(lname === "")
+		{
+			flag = false;
+			document.getElementById("lnameErr").innerHTML = "Last Name can't be empty!";
+		}
+
+		if(gender === "")
+		{
+			flag = false;
+			document.getElementById("genderErr").innerHTML = "Gender can't be empty!";
+		}
+
+		if(dob === "")
+		{
+			flag = false;
+			document.getElementById("dobErr").innerHTML = "Date of Birth can't be empty!";
+		}
+
+		if(religion === "")
+		{
+			flag = false;
+			document.getElementById("religionErr").innerHTML = "Religion can't be empty!";
+		}
+
+		if(email === "")
+		{
+			flag = false;
+			document.getElementById("emailErr").innerHTML = "Email can't be empty!";
+		}
+
+		if(username === "")
+		{
+			flag = false;
+			document.getElementById("usernameErr").innerHTML = "Username can't be empty!";
+		}
+
+		if(password === "")
+		{
+			flag = false;
+			document.getElementById("passwordErr").innerHTML = "Password can't be empty!";
+		}
+
+	
+		return flag;
+
 	}
 </script>
 
